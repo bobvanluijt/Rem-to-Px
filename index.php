929fdbb16bf02fe2e8010dda3d19de24c0838ed8
bobvanluijt <bob@vanluijt.nl>
2014-10-21 16:59:18 +0200

    Updates for live version

--- index.php
+++ index.php
@@ -20,21 +20,24 @@
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 if(isset($_GET['u'])){
-	$css['file'] =  file_get_contents($_GET['u']);
+	header("Content-type: text/css");
+	
+	$css['file']['old'] = file_get_contents($_GET['u']);
+	$css['file']['new'] = $css['file']['old'];
 	$css['length'] = strlen($css['file']);
 	//
 	//find the html { } font-size value;
 	//
-	$css['foundHTMLs'] = substr_count($css['file'], 'html');
+	$css['foundHTMLs'] = substr_count($css['file']['old'], 'html');
 	$countRow=0;
 	$countOcc=0;
 	while($countOcc<$css['foundHTMLs']){
 		$output['received']['html'] = '';
-		$countRow = strpos($css['file'], 'html', $countRow);
+		$countRow = strpos($css['file']['old'], 'html', $countRow);
 		$output['locationCounter'] = $countRow; 
 		$output['step']='';
 		while(substr($output['step'],-1)!='}'){
-			$output['step'] = substr($css['file'], $output['locationCounter']++, 1);
+			$output['step'] = substr($css['file']['old'], $output['locationCounter']++, 1);
 			$output['received']['html'] .= $output['step'];
 		}
 		if(strpos($output['received']['html'], 'font-size')!==false){
@@ -52,18 +55,17 @@ if(isset($_GET['u'])){
 	} else {
 		$output['font-size']['type'] = 'px';
 	}
-	var_dump($output['font-size']);
-	exit;
 	
 	//
 	//find all the rems including their locations
 	//
 	$needle = 'rem';
-	$css['foundRems'] = substr_count($css['file'], $needle);
+	$css['foundRems'] = substr_count($css['file']['old'], $needle);
 	$countRow=0;
 	$countOcc=0;
+	$i=0;
 	while($countOcc<$css['foundRems']){
-		$countRow = strpos($css['file'], $needle, $countRow);
+		$countRow = strpos($css['file']['old'], $needle, $countRow);
 		//walk left and find the end
 		$output['received']=array();
 		$output['step']='';
@@ -71,10 +73,8 @@ if(isset($_GET['u'])){
 		$output['locationCounter'] = $output['location'];
 		$output['i']=0;
 		while(substr($output['step'],0,1)!=';' && substr($output['step'],0,1)!='{'){
-			$output['step'] = substr($css['file'], $output['locationCounter']--, 1);
-			//if(is_numeric($output['step']) || $output['step']=='.'){
-			//	$output['received'] = $output['step'].$output['received'];
-			//}
+			$locationForOutput = $output['locationCounter']; //this contains the start locations
+			$output['step'] = substr($css['file']['old'], $output['locationCounter']--, 1);
 			$output['received']['pre'] = $output['step'].$output['received']['pre'];
 		}
 		$output['received']['pre'] = substr(substr($output['received']['pre'], 1), 0, -1);
@@ -83,18 +83,53 @@ if(isset($_GET['u'])){
 		$output['step']='';
 		$output['locationCounter'] = $output['location'];
 		while(substr($output['step'],-1)!=';'){
-			$output['step'] = substr($css['file'], $output['locationCounter']++, 1);
+			$output['step'] = substr($css['file']['old'], $output['locationCounter']++, 1);
 			$output['received']['post'] = $output['received']['post'].$output['step'];
 		}
 		
-		$output['received'] = $output['received']['pre'].$output['received']['post'];
+		$valForOutput = $output['received']['pre'].$output['received']['post'];
 		
-		//echo $output['received'].'<br>';
+		//this contains the start locations
+		$outputReplace[$i]['location'] = $locationForOutput;
+		$outputReplace[$i]['val'] = $valForOutput;
+		$i++;
 		
 		$countRow++;
 		$countOcc++;
 	}
 	
+	//
+	//change REMs to PX
+	//
+	foreach($outputReplace as $val){
+		$i=0;
+		$posOffset=0;
+		
+		$occ = substr_count($val['val'], $needle);
+		while($i<$occ){
+			$pos = strpos($val['val'], $needle, $posOffset++);
+			
+			//go left to find val
+			$remValOutput='';
+			while(1==1){
+				$pos--;
+				$remVal = substr($val['val'], $pos, 1);
+				if(is_numeric($remVal) || $remVal=='.'){
+					$remValOutput = $remVal.$remValOutput;
+				} else {
+					break;	
+				}
+			}
+			
+			$defOutputPre = $remValOutput.$needle;
+			$defOutputPost = (float)$remValOutput*$output['font-size']['val'];
+			$defOutputPost .= $output['font-size']['type'];
+			$css['file']['new'] = str_replace($defOutputPre, $defOutputPost, $css['file']['new']);
+			$i++;
+		}
+	}
 	
-	exit;	
+	echo $css['file']['new'];
+	
+	exit;
 }
